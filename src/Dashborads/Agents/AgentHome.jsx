b3106d9da197041075ca5b824a9a5a38ff5966d9
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-07-19 12:22:22 +0200

    screen size for small devices has been updated

--- src/Dashborads/Agents/AgentHome.jsx
+++ src/Dashborads/Agents/AgentHome.jsx
@@ -1,37 +1,4 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
 
-// const AgentHome = () => {
-//     return (
-//         <div>
-//             <div className="flex flex-col gap-4 mt-4">
-//                 <h1 className='text-center'>Agent Home</h1>
-//                 <div className="flex  justify-center items-center gap-4">
-//                     <div className="w-[200px] bg-slate-700">
-//                         <Link to='/dashboard/agentCurrentBalance'><h2 className="text-center">Current Balance</h2></Link>
-                        
-//                     </div>
-//                     <div className="w-[200px] bg-blue-400">
-//                         <Link to='agentTransactionManagement'><h2 className="text-center"> Transaction management</h2></Link>
-//                     </div>
-//                 </div>
-//                 <div>
-                  
-//                     <div className="flex w-[200px] mx-auto justify-center items-center bg-slate-700 mt-4">
-//                         <Link to='agentTransactionHistory'>   <h1> Transaction  History</h1></Link>
-
-//                     </div>
-//                 </div>
-
-                
-
-//             </div>
-            
-//         </div>
-//     );
-// };
-
-// export default AgentHome;
 
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -49,15 +16,13 @@ const AgentHome = () => {
                 <div className="flex justify-center items-center gap-8">
                     <div className="w-[100px] bg-blue-400 rounded-2xl p-2">
                         <Link to={'/agentCurrentBalance'}>
-                            {/* <RiMoneyEuroCircleLine className='text-4xl text-yellow-400 mx-auto' /> */}
-                            <img src={balance} alt="" />
+                           <img src={balance} alt="" />
                             <h2 className="text-center text-white">Balance</h2></Link>
                     </div>
                     <div className="w-[100px] bg-blue-400 rounded-2xl p-2">
                         
                         <Link to={'/agentTransactionManagement'}>
-                            {/* <GrTransaction className='text-4xl mx-auto' /> */}
-                            <img src={transaction} alt="" />
+                          <img src={transaction} alt="" />
                             <h2 className="text-center text-white">Transaction</h2></Link>
                     </div>
                 </div>
@@ -74,4 +39,4 @@ const AgentHome = () => {
 };
 
 export default AgentHome;
-import { RiMoneyEuroCircleLine } from "react-icons/ri";
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,79 +1,4 @@
-// import React, { useEffect, useState } from 'react';
-// import {  useNavigate } from 'react-router-dom';
 
-// const Header = () => {
-//     const [userRole, setUserRole] = useState();
-//     const navigate = useNavigate();
-
-
-//     useEffect(() => {
-//         const role = localStorage.getItem('userRole');
-//         if (role) {
-//             setUserRole(role);
-//         }
-//     }, [userRole]);
-//     const handleLogOut = () => {
-//         localStorage.removeItem('token'); // Remove JWT token from localStorage
-//         localStorage.removeItem('userID'); // Remove userRole from localStorage
-//         setUserRole(null); // Update state for immediate UI change
-//         navigate('/'); // Redirect to login after logout
-//     };
-//     return (
-//         <div className="bg-blue-500 h-[100px]">
-//             <h1 className="text-center">Hello {userRole ? userRole : ''}</h1>
-//             <div className="flex float-end">
-//                 {userRole && (
-//                     <button onClick={handleLogOut} className="bg-red-500 w-[76px] h-6 text-white rounded-xl font-bold">LogOut</button>
-//                 )}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Header;
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
-
-// const Header = () => {
-//     const [userRole, setUserRole] = useState(null);
-//     const navigate = useNavigate();
-
-//     useEffect(() => {
-//         const role = localStorage.getItem('userRole');
-//         if (role) {
-//             setUserRole(role);
-//         }
-//     }, []);
-
-//     const handleLogOut = () => {
-//         localStorage.removeItem('token'); // Remove JWT token from localStorage
-//         localStorage.removeItem('userID'); // Remove userID from localStorage
-//         localStorage.removeItem('userRole'); // Remove userRole from localStorage
-//         setUserRole(null); // Update state for immediate UI change
-//         navigate('/'); // Redirect to login after logout
-//     };
-
-//     return (
-//         <div className="bg-blue-500 h-[100px]">
-//             <h1 className="text-center">Hello {userRole ? userRole : ''}</h1>
-//             <div className="flex float-end">
-//                 {userRole && (
-//                     <button onClick={handleLogOut} className="bg-red-500 w-[76px] h-6 text-white rounded-xl font-bold">LogOut</button>
-//                 )}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Header;
-
-
-
-
-// src/components/Header.jsx
 import React, { useContext } from 'react';
 import { AuthContext } from '../components/AuthContext'; // Adjust the path as necessary
 import logo from '../assets/tkLogo.png'
@@ -82,14 +7,10 @@ const Header = () => {
     
 
     return (
-        <div className="bg-blue-500  w-[576px] mx-auto rounded-xl flex flex-col justify-center items-center ">
+        <div className="bg-blue-500  lg:w-[576px] mx-auto rounded-xl flex flex-col justify-center items-center ">
             <img className='w-12 h-12' src={logo} alt="" />
             <h1 className="text-center">Taka</h1>
-            {/* <div className="flex float-end">
-                {userRole && (
-                    <button onClick={handleLogOut} className="bg-red-500 w-[76px] h-6 text-white rounded-xl font-bold">LogOut</button>
-                )}
-            </div> */}
+            
         </div>
     );
 };
