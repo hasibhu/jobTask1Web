414f0fa8d128830de047d8447734f4423e32629b
hasibhu <hasibul1987@gmail.com>
2024-08-11 11:34:43 +0200

    User History component has been adjusted 2

--- src/Dashborads/Users/UserHistory.jsx
+++ src/Dashborads/Users/UserHistory.jsx
@@ -4,6 +4,9 @@ const UserHistory = () => {
     return (
         <div>
             <h1 className='text-center'>All user payments willll be here .... </h1>
+            <h2>New line
+                
+            </h2>
         </div>
     );
 };
